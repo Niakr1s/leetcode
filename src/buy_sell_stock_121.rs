c2b2943bc5bfd25b3bf3bf9174a2654fd67fbe6d
Niakr1s <pavel2188@gmail.com>
2023-08-11 14:09:29 +0300

    finished 121 solutino

--- src/buy_sell_stock_121.rs
+++ src/buy_sell_stock_121.rs
@@ -1,5 +1,17 @@
 pub fn max_profit(prices: Vec<i32>) -> i32 {
-    max_profit_naive(prices)
+    if prices.is_empty() {
+        return 0;
+    }
+    
+    let mut min: i32 = prices[0];
+    let mut profit: i32 = 0;
+    for price in prices {
+        if price < min {
+            min = price;
+        }
+        profit = profit.max(price - min);
+    }
+    profit
 }
 
 fn max_profit_naive(prices: Vec<i32>) -> i32 {
