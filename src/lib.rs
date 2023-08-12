e059b0cf3452ce9ed408b4b22f2132aa534d2e5f
Niakr1s <pavel2188@gmail.com>
2023-08-12 22:08:03 +0300

    fixed lf

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,7 @@
 #![allow(dead_code)]
 
-pub mod two_sum_1;
-pub mod longest_substring_3;
-pub mod valid_parentheses_20;
 pub mod buy_sell_stock_121;
+pub mod longest_substring_3;
 pub mod search_in_rrotated_sort_arr_33;
+pub mod two_sum_1;
+pub mod valid_parentheses_20;
